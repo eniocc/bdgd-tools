849abe022bddc7a25d9b699383f2b088ba40a4d2
Ênio <eniocc@gmail.com>
2022-09-19 18:34:05 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,6 +37,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/eniocc/bdgd_tools',
-    version='0.2.1',
+    version='0.2.3',
     zip_safe=False,
 )
