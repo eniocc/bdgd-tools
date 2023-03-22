69a7cf4d0194c5940872ea9dcce0b54e47fa0a8e
Ênio Viana <eniocc@gmail.com>
2023-03-22 10:40:54 -0300

    Sample Structure

---
+++ bdgd_tools/sample/Sample.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+"""
+ * Project Name: bdgd-tools
+ * Created by eniocc
+ * Date: 22/03/2023
+ * Time: 10:39
+ *
+ * Edited by: eniocc
+ * Date: 22/03/2023
+ * Time: 10:39
+"""
