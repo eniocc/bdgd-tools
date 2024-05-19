c67425fafffe6522f0fa9da03699dfaaffdaeaef
Raphael Toshio Sakai <raphaeltoshio@usp.br>
2024-05-19 11:19:32 -0300

    Breve alteração no arquivo bdgd_tools.py e no requirements.txt.

--- bdgd_tools.py
+++ bdgd_tools.py
@@ -15,4 +15,4 @@ import bdgd_tools as bdgd
 
 if __name__ == '__main__':
 
-    bdgd.run(r"C:\Users\ADERALDO\Desktop\TCC BDGB\CRELUZ-D_598_2022-12-31_V11_20230831-0921.gdb", feeder="1_3PAS_1")
+    bdgd.run(r"C:\Raphael_Projetos\BDGD_tools_RTS_fork\bdgd-tools-RTS\bdgd_tools\sample\raw\aneel\CRELUZ-D_598_2022-12-31_V11_20230831-0921.gdb", feeder="1_3PAS_1")
