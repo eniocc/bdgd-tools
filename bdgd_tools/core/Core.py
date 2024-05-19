41f563ce57a1c18cdf318421419f80eb9ca1ae1e
Raphael Toshio Sakai <raphaeltoshio@usp.br>
2024-05-19 11:46:46 -0300

    Tirei os coisinhos que estavam atrapalhando que o próprio sistema colocou.

--- bdgd_tools/core/Core.py
+++ bdgd_tools/core/Core.py
@@ -176,11 +176,8 @@ def run(folder: Optional[str] = None, feeder: Optional[str] = None,  all_feeders
     json_data = JsonData(json_file_name)
 
     geodataframes = json_data.create_geodataframes(folder_bdgd)
-<<<<<<< HEAD
+
     #df_coords = buscoords.coords()
-=======
-    df_coords = buscoords.coords()
->>>>>>> 3baf3072225c57918f61fc18d7305f88ae59d86f
 
     # #! Modificado
     # run_gui(folder_bdgd, feeder)
@@ -240,14 +237,8 @@ def run(folder: Optional[str] = None, feeder: Optional[str] = None,  all_feeders
             case.output_master(list_files_name)
             case.create_outputs_masters(list_files_name)
 
-
-<<<<<<< HEAD
             #folder_output = r'C:\\bdgd-tools-main\\output\\'
             #df_coords.to_csv(f'{folder_output}coords.csv', index=False)
             #print ('teste')
-=======
-            folder_output = r'C:\\bdgd-tools-main\\output\\'
-            df_coords.to_csv(f'{folder_output}coords.csv', index=False)
-            print ('teste')
->>>>>>> 3baf3072225c57918f61fc18d7305f88ae59d86f
+
 
